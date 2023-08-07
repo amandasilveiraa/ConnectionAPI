7a0cc19eb39a2c4fef1134f2b65a63db3acd071d
amandadrehmer <amandasilveira.rs@gmail.com>
2023-08-07 10:02:03 -0300

    Adicionando o código da API de Rick and Morty

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function App () {
+  const [characterDados, setCharacterDados] = useState(null); // Para armazenar os dados do personagem obtidos da API
+
+  function fetchRandomCharacter() { // Para buscar um personagem aleatório da API
+    
+    const randomId = Math.floor(Math.random() * 671) + 1; // Calcula um ID aleatório (entre 1 e 671, pois a API tem 671 personagens) para buscar um personagem específico
+
+    fetch(`https://rickandmortyapi.com/api/character/${randomId}`) // Para realizar uma solicitação à API "Rick and Morty".
+      .then((response) => response.json()) // Para converter a resposta em formato JSON.
+      .then((dados) => setCharacterDados(dados)) // Para atualizar o estado characterData com os dados do personagem obtidos da API
+
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Informações de Personagem Rick and Morty</h1>
+      <button onClick={fetchRandomCharacter}>Gerar Personagem</button>
+
+      {/* && = Para exibir os dados do personagem somente quando o estado characterData não for null. Se characterData contiver dados, mostra a tag <div> com a imagem do personagem, seu nome, espécie, status, gênero e origem. */}
+      {characterDados && ( 
+        <div className="character-info">
+          <img src={characterDados.image} alt={characterDados.name} />
+          <h2>{characterDados.name}</h2>
+          <p>Espécie: {characterDados.species}</p>
+          <p>Status: {characterDados.status}</p>
+          <p>Gênero: {characterDados.gender}</p>
+          <p>Origem: {characterDados.origin.name}</p>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
